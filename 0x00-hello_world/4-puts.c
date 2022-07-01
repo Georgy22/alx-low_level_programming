17495085d3063c5c3e9971e47aefd2d76d25a045
Georgy22 <georgi.reginald12@gmail.com>
2022-06-30 21:22:47 -0700

    he

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,11 @@
 #include <stdio.h>
-
 /**
- *  * main - Entry point
- *   * print word to output
- *    * Return: Always 0 (Success)
+ *  * main - entry point
+ *   *
+ *    * Return: 0 after printing the function
  *     */
-
 int main(void)
 {
-	    puts("\"Programming is like building a multilingual puzzle");
-	        return (0);
+		puts("\"Programming is like building a multilingual puzzle");
+			return (0);
 }
