d8ea76badf58c84b3af027223ba0ed0ea77f5acf
Georgy22 <106970884+Georgy22@users.noreply.github.com>
2022-07-02 06:00:23 +0100

    Hi

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,19 +1,22 @@
 #include <stdio.h>
 /**
-* main - Entry point
-* Return: Always 0 (Success/correct)
-*/
+ * main - main block
+ * Description: Get a random number and check its last digit, compare it with 5
+ * Return: 0
+ */
 int main(void)
 {
-        char alphabet;
-        for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
-        {
-        if (alphabet == 'q')
-        continue;
-        else if (alphabet == 'e')
-        continue;
-        putchar(alphabet);
-        }
-        putchar('\n');
-        return (0);
+	char c = 'a';
+
+	while (c <= 'z')
+	{
+		if (c != 'e' && c != 'q')
+		{
+			putchar(c);
+		}
+		c++;
+	}
+
+	putchar('\n');
+	return (0);
 }
