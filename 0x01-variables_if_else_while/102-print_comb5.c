270565d7c7c473a1bec6fcd55080bda41e1de7da
Georgy22 <106970884+Georgy22@users.noreply.github.com>
2022-07-02 06:24:50 +0100

    Done

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,29 +1,46 @@
 #include <stdio.h>
+#include <stdlib.h>
 /**
-* main - Entry point
-* Return: Always 0 (success/correct)
-*/
+ * main -mainblock to e executed
+ * Descripton: program that prints all possible combinations of digits
+ * Return: 0
+ */
 int main(void)
 {
-        int first_nums;
-        int second_nums;
-        for (first_nums = 0; first_nums < 100 ; first_nums++)
-        {
-        for (second_nums = first_nums + 1 ; second_nums < 100; second_nums++)
-        {
-        putchar (first_nums / 10 + '0');
-        putchar (first_nums % 10 + '0');
-        putchar (' ');
-        putchar (second_nums / 10 + '0');
-        putchar (second_nums % 10 + '0');
-        if (first_nums == 98 && second_nums == 99)
-        {
-        break;
-        }
-        putchar(',');
-        putchar (' ');
-        }
-        }
-        putchar('\n');
-        return (0);
+int c = 0;
+int f_d;
+int l_d;
+
+int c2;
+int f_d2;
+int l_d2;
+
+while (c <= 98)
+{
+	f_d = (c / 10 + '0');
+	l_d = (c % 10 + '0');
+	c2 = 0;
+	while (c2 <= 99)
+	{
+		f_d2 = (c2 / 10 + '0');
+		l_d2 = (c2 % 10 + '0');
+		if (c < c2)
+		{
+			putchar(f_d);
+			putchar(l_d);
+			putchar(' ');
+			putchar(f_d2);
+			putchar(l_d2);
+			if (c != 98)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+		c2++;
+	}
+	c++;
+}
+putchar('\n');
+return (0);
 }
