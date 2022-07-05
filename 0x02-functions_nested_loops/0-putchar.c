9b29ccb896040ac245350d62f32d7110c9cc138d
Georgy22 <106970884+Georgy22@users.noreply.github.com>
2022-07-05 21:44:12 +0100

    Sleep

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,21 +1,13 @@
+#include <stdio.h>
+
+
 /**
- * main - prints putchar
- * Return: always 0
+ * main -Entry point of the program
+ *
+ * Return: Always 0 (Success)
  */
-
-
 int main(void)
 {
-_putchar('_');
-_putchar('p');
-_putchar('u');
-_putchar('t');
-_putchar('c');
-_putchar('h');
-_putchar('a');
-_putchar('r');
-_putchar('\n');
-
-
-return (0);
+	printf("_putchar\n");
+	return (0);
 }
