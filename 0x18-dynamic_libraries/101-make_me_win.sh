a0c7197cda6805b04b9f4a5ef9dbef65e521356a
Georgy22 <georgi.reginald12@gmail.com>
2022-10-06 00:41:42 -0700

    dynamic libraries

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,4 +1,3 @@
-
 #!/bin/bash
-wget -P /tmp/ https://raw.github.com/Natzsmart/alx-low_level_programming/main/0x18-dynamic_libraries/preload.so
-export LD_PRELOAD=/tmp/preload.so
+wget -P /tmp https://github.com/Tolulope05/alx-low_level_programming/raw/master/0x18-dynamic_libraries/nrandom.so
+export LD_PRELOAD=/tmp/nrandom.so
